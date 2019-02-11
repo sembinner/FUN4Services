597eb6269dbfca81f0b9b7e6ce5031e8a514b6a8
Binner, Sem S.A.F <sem@binner.nl>
2019-02-11 15:16:11 +0100

    Added test endpoint to usercontroller

--- user-service/src/main/java/com/fun4/services/controllers/UserController.java
+++ user-service/src/main/java/com/fun4/services/controllers/UserController.java
@@ -1,4 +1,14 @@
 package com.fun4.services.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "Server is online!";
+    }
 }
