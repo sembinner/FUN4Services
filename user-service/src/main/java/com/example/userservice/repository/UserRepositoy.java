3b0a6e0b4667bfb782a416cb7277309c49d08c3e
Mike Manders <mikemanders1999@gmail.com>
2019-02-18 11:59:57 +0100

    User with UUID (we create the UUID, not Hibernate)

--- user-service/src/main/java/com/example/userservice/repository/UserRepositoy.java
+++ user-service/src/main/java/com/example/userservice/repository/UserRepositoy.java
@@ -4,6 +4,8 @@ import com.example.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepositoy extends JpaRepository<User, Long> {
+public interface UserRepositoy extends JpaRepository<User, String> {
 }
