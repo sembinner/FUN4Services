cf87334a836efacb8196f36d92aa34c4daa3f38b
sembinner <sem.binner@outlook.com>
2019-03-18 13:38:01 +0100

    userservice tests

--- user-service/src/test/java/com/fun4/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/fun4/userservice/UserServiceApplicationTests.java
@@ -1,5 +1,9 @@
 package com.fun4.userservice;
 
+import com.fun4.userservice.manager.UserManager;
+import com.fun4.userservice.model.User;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +12,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceApplicationTests {
+    private UserManager manager;
+
+    @Before
+    public void setUp() throws Exception {
+        this.manager = new UserManager();
+    }
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void testGetUser() {
+        User user = this.manager.getUserByUsername("test");
+        Assert.assertNotNull(user);
+    }
+
+    @Test
+    public void testIfThrowsErrorIfUserDoesNotExist() {
+        User user = this.manager.getUserByUsername("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        Assert.assertNull(user);
+    }
 }
