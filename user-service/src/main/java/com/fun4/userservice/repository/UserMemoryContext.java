db9def9ba720e5d677908d4333794573672c5297
Sem Binner <sem.binner@outlook.com>
2019-03-19 11:21:47 +0100

    Added some tests and memorycontext

--- user-service/src/main/java/com/fun4/userservice/repository/UserMemoryContext.java
+++ user-service/src/main/java/com/fun4/userservice/repository/UserMemoryContext.java
@@ -2,14 +2,27 @@ package com.fun4.userservice.repository;
 
 import com.fun4.userservice.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class UserMemoryContext implements IUserRepository {
+    private List<User> users = new ArrayList<>();
+
+    public UserMemoryContext() {
+        users.add(new User(100, "test@test.nl", "test", "tester", "test", "test"));
+    }
+
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        return users.stream().filter(x -> x.getUsername().equals(username)).findFirst().orElse(null);
     }
 
     @Override
     public User addUser(User user) {
-        return null;
+        Random random = new Random();
+        user.setId(random.nextInt(1000));
+        users.add(user);
+        return user;
     }
 }
--- user-service/src/test/java/com/fun4/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/fun4/userservice/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.fun4.userservice;
 
 import com.fun4.userservice.manager.UserManager;
 import com.fun4.userservice.model.User;
+import com.fun4.userservice.repository.UserMemoryContext;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +14,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceApplicationTests {
-    private UserManager manager;
+    private static UserManager manager;
 
-    @Before
-    public void setUp() throws Exception {
-        this.manager = new UserManager();
+    @BeforeClass
+    public static void setUp() throws Exception {
+        manager = new UserManager(new UserMemoryContext());
     }
 
     @Test
@@ -33,5 +35,37 @@ public class UserServiceApplicationTests {
     public void testIfThrowsErrorIfUserDoesNotExist() {
         User user = this.manager.getUserByUsername("aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         Assert.assertNull(user);
+    }
+
+    @Test
+    public void testAddUser() {
+        try {
+            this.manager.addUser(new User("user@user.nl", "newuser", "firstnameUser", "lastnameUser", "passwordUser"), "passwordUser");
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+        User user = manager.getUserByUsername("newuser");
+        Assert.assertNotNull(user);
+    }
+
+    @Test
+    public void testAddUserNameToLowerCase() {
+        try {
+            this.manager.addUser(new User("user@user.nl", "userTWO", "firstnameUser", "lastnameUser", "passwordUser"), "passwordUser");
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
+        User user = manager.getUserByUsername("usertwo");
+        Assert.assertNotNull(user);
+    }
+    @Test
+    public void testIfThrowsErrorIfUserNameAlreadyExists() {
+        try {
+            this.manager.addUser(new User("user@user.nl", "duplicateUser", "firstnameUser", "lastnameUser", "passwordUser"), "passwordUser");
+            this.manager.addUser(new User("user@user.nl", "duplicateUser", "firstnameUser", "lastnameUser", "passwordUser"), "passwordUser");
+        } catch (Exception e) {
+            return;
+        }
+        Assert.fail();
     }
 }
